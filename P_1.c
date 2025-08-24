35a3546b5c6fa7ea88037a10071501ea1593f721
gohilparikshitsinh48 <gohilparikshitsinh48@gmail.com>
2025-08-24 11:53:06 +0530

    Update P_1.c

--- P_1.c
+++ P_1.c
@@ -9,7 +9,7 @@ printf("Enter value of a: ");
 scanf("%d", &a);
 
 printf("Enter value of b: ");
-
+scanf("%d",&b);
 if(a<=b)
 {
 
@@ -17,10 +17,18 @@ printf("a is smallest number");
  }
 else
 { 
-
 printf("b is smallest number");
 
 }
+ 
+if(a>=b)
+{
+ printf("a is largest number");
+  }
+  else
 
+   {
+    printf("b is largest number");
+   }
 return 0;
 }
